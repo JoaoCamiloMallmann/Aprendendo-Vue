f411b7aedfc27d190defc8592bd8efa93e7b13b0
João <joao@liquidworks.com.br>
2022-12-29 08:54:16 -0300

    .

---
+++ Vue/projeto/src/components/Lifecycle.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>meu nome é :{{ nome }}</h1>
+</template>
+
+<script>
+export default {
+  name: "LifeCycle",
+
+  data() {
+    return {
+      nome: "ainda nao sei",
+    };
+  },
+  created() {
+    this.nome = "mario";
+  },
+  mounted() {
+    setTimeout(() => {
+      this.nome = "jose";
+    }, 1000);
+  },
+};
+</script>
---
+++ Vue/projeto/src/components/PrimeiroComponente.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Olá Vue</h1>
+  <p>Meu nome é {{ nome }} e sou {{ trabalho }}</p>
+</template>
+
+
+<script>
+export default {
+  name: "PrimeiroComponente",
+
+  data() {
+    return {
+      nome: "joao",
+      trabalho: "Programador",
+    };
+  },
+};
+</script>
