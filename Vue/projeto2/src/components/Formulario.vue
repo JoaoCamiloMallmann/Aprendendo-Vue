9e566650b473ee40b48caa2f9b9f03d1d07109ed
João <joao@liquidworks.com.br>
2023-01-03 16:43:16 -0300

    .

---
+++ Vue/projeto2/src/components/Formulario.vue
@@ -0,0 +1,55 @@
+<template>
+  <span></span>
+  <form action="POST" id="formulario">
+    <div class="formularioParte">
+      <label for="name"> Nome: </label>
+      <input type="text" name="name" v-model="nome" />
+    </div>
+
+    <div class="formularioParte">
+      <label for="pao"> Pão </label>
+      <select name="pai" id="pao" v-model="pao">
+        <option value="">Escolha um pão</option>
+        <option value=""></option>
+      </select>
+    </div>
+
+    <div class="formularioParte">
+      <label for="carne"> Carne </label>
+      <select name="pai" id="carne" v-model="carne">
+        <option value="">Escolha um carne</option>
+        <option value=""></option>
+      </select>
+    </div>
+
+    <div class="formularioParte">
+      <button type="submit">Enviar</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "Formulario",
+  data() {
+    return {
+      nome: "",
+      pao: "",
+      carne: "",
+    };
+  },
+};
+</script>
+
+<style scoped>
+#formulario {
+  display: flex;
+  flex-direction: column;
+}
+.formularioParte {
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  margin: 1rem 0;
+}
+</style>
--- Vue/projeto2/src/views/HomeView.vue
+++ Vue/projeto2/src/views/HomeView.vue
@@ -1,17 +1,21 @@
 <template>
+  <Banner />
   <div class="mainContainer">
-    <Banner />
     <h1>Home</h1>
+
+    <Formulario />
   </div>
 </template>
 
 <script>
 import Banner from "../components/Banner.vue";
+import Formulario from "../components/Formulario.vue";
 
 export default {
   name: "HomeView",
   components: {
     Banner,
+    Formulario,
   },
 };
 </script>
